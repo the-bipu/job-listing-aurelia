48c2cce4540b41cb04f4a6023eb11481a1ae80cc
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-08-25 15:20:21 +0530

    complete project

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,41 @@
 import data from '../static/data.json';
 
 export class App {
-  public message = 'Hello World!';
-  public data = data;
+  public data: typeof data = data;
+
+  public clickedItems: { item: string, type: string }[] = [];
+
+  public logItem(item: string, type: 'language' | 'tool'): void {
+    const exists = this.clickedItems.some(
+      clickedItem => clickedItem.item === item && clickedItem.type === type
+    );
+
+    if (!exists) {
+      this.clickedItems.push({ item, type });
+    }
+  }
+
+  public removeItem(clickedItem: { item: string, type: string }): void {
+    this.clickedItems = this.clickedItems.filter(
+      item => item.item !== clickedItem.item || item.type !== clickedItem.type
+    );
+  }
+
+  public get filteredData() {
+    if (this.clickedItems.length === 0) {
+      return this.data;
+    }
+
+    return this.data.filter(card => {
+      return this.clickedItems.every(clickedItem => {
+        if (clickedItem.type === 'language') {
+          return card.languages.includes(clickedItem.item);
+        } else if (clickedItem.type === 'tool') {
+          return card.tools.includes(clickedItem.item);
+        }
+        return false;
+      });
+    });
+  }
+
 }
